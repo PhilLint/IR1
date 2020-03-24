e784e1a47a51a58b02af1ee36de15a614cb5a66d
CaitlinBr <60697462+CaitlinBr@users.noreply.github.com>
2020-03-24 19:34:39 +0100

    Pairwise and pairwise SU updated

--- hw3/pairwise.py
+++ hw3/pairwise.py
@@ -4,9 +4,7 @@ import evaluate as evl
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
-
 from scipy import stats
-from torch import nn, optim
 from torch.utils.data import Dataset, TensorDataset, DataLoader
 import itertools
 
@@ -14,65 +12,151 @@ import math
 data = dataset.get_dataset().get_data_folds()[0]
 data.read_data()
 
+# RankNet
+
+class RankNet(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden):
+        super(RankNet, self).__init__()
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)
+        self.output = torch.nn.Linear(n_hidden, 1)      
+        
+    def forward(self, x1):
+        x = torch.nn.functional.relu(self.hidden(x1))
+        x = self.output(x)
+        
+        return x
 
 class Model():
     def __init__(self, n_feature, n_hidden, learning_rate, sigma):
-        self.ranknet = nn.Sequential(
-                                nn.Linear(n_feature, n_hidden),
-                                nn.ReLU(),
-                                nn.Linear(n_hidden, 1)
-                                )
-        self.optimizer = optim.SGD(self.ranknet.parameters(), lr=learning_rate)
-
-
+        self.ranknet = RankNet(n_feature, n_hidden)
+        self.optimizer = torch.optim.SGD(self.ranknet.parameters(), lr=learning_rate)
 
 def eval_model(model, data_fold):
-    x = torch.from_numpy(data_fold.feature_matrix).float()
-    y = data_fold.label_vector
+        x = torch.from_numpy(data_fold.feature_matrix).float()
+        y = data_fold.label_vector
+        model.ranknet.eval()
+               
+        output = model.ranknet(x)
+        output = output.detach().cpu().numpy().squeeze()
+        
+        scores = evl.evaluate(data_fold, np.asarray(output))  
+
+        return scores
+
+
+def load_dataset():
+
+    train_x = torch.from_numpy(data.train.feature_matrix).float()
+    train_y = torch.from_numpy(data.train.label_vector).float()
+
+    train_set = TensorDataset(train_x, train_y)
+    train_loader = DataLoader(dataset=train_set, batch_size=32, shuffle=True)
+
+    return train_loader
+       
+
+def plot_ndcg_loss(ndcgs):
+    x = np.arange(len(ndcgs))
+    fig, ax = plt.subplots()
+    
+    ax.plot(x, ndcgs, label='NDCG')
+    ax.set_xlabel("Batch % 2000")
+    ax.set_ylabel("Score")
+    ax.set_title("Pairwise LTR")
+    legend = ax.legend(loc='upper center')
+    
+    plt.show()
+    plt.savefig('Pairwise_LTR_plot.png')
+
+    
+def get_distributions(model):
+    data = dataset.get_dataset().get_data_folds()[0]
+    data.read_data()
     model.ranknet.eval()
+
+    val_x = torch.from_numpy(data.validation.feature_matrix).float()
+    test_x = torch.from_numpy(data.test.feature_matrix).float()
            
-    output = model.ranknet(x)
-    output = output.detach().cpu().numpy().squeeze()
+    val = model.ranknet(val_x).detach().cpu().numpy().squeeze()
+    test = model.ranknet(test_x).detach().cpu().numpy().squeeze()
+    actual = np.concatenate((data.train.label_vector, data.validation.label_vector, data.test.label_vector))
+    
+    distributions = {
+    "val_mean": np.mean(val),
+    "val_std": np.std(val),
+    "test_mean": np.mean(test),
+    "test_std": np.std(test),
+    "actual_mean": np.mean(actual), 
+    "actual_std": np.std(actual),
+    }
+    
+    return distributions
 
-    scores = evl.evaluate(data_fold, np.asarray(output))  
-
-    return scores      
 
     
 def train_batch(documentfeatures, labels, model, sig):
-    
-    model.ranknet.train()
-    model.optimizer.zero_grad()
-    
-    output = model.ranknet(documentfeatures)
-    
-    loss = pairwiseloss(output, labels, sig)
-    
-    loss.backward()
+    for epoch in range(1):
+      model.optimizer.zero_grad()
 
-    model.optimizer.step()
+      output = model.ranknet(documentfeatures)
+      
+      loss = pairwiseloss(output, labels, sig)
+  
+      loss.backward()
+      
+      model.optimizer.step()
             
     return model
     
+def pairwiseloss(predictedvals, values, sigma):
     
-def pairwiseloss(preds, labels, sigma):
-    preds = preds.squeeze()
+    predictedvals = predictedvals.squeeze()
 
-    pairs = list(itertools.combinations(range(preds.shape[0]), 2))
-    idx1, idx2 = [pair[0] for pair in pairs], [pair[1] for pair in pairs]
-   
-    S = torch.sign(labels[idx1] - labels[idx2])
-    s = preds[idx1] - preds[idx2]   
+    if values.shape[0] == 1:
+        return (predictedvals - values)**2
         
-    C = 0.5 * (1 - S) * sigma * s + torch.log(1 + torch.exp(-sigma * s))
+    pairs = list(itertools.combinations(range(predictedvals.shape[0]), 2))
+    val1, val2 = [x[0] for x in pairs], [x[1] for x in pairs]
+   
+    S = torch.sign(values[val1] - values[val2])
+    s = predictedvals[val1] - predictedvals[val2]   
+        
+    C_T = 0.5 * (1 - S) * sigma * s + torch.log(1 + torch.exp(-sigma * s))
  
-    return C.sum()   
+    return C_T.sum()
 
+
+def train_best(best_params):
+    epochs = best_params["epoch"]
+    n_hidden = best_params["n_hidden"]
+    learning_rate = best_params["learning_rate"]
+    sigma = best_params["sigma"]
+    
+    model = Model(data.num_features, n_hidden, learning_rate, sigma)
+
+    losses, ndcgs = [], []
+    for epoch in range(epochs):
+        eval_count = 0
+        for qid in range(0, data.train.num_queries()):
+            if data.train.query_size(qid) < 2:
+                continue
+            s_i, e_i = data.train.query_range(qid)
+            
+            documentfeatures = torch.tensor(data.train.feature_matrix[s_i:e_i]).float()
+            labels = torch.tensor(data.train.label_vector[s_i:e_i])
+            model = train_batch(documentfeatures, labels, model, sigma) 
+            eval_count +=1
+            if eval_count % 2000 == 0:
+                scores = eval_model(model, data.validation)
+                ndcgs.append(scores["ndcg"][0])
+        print("Epoch: {}, ndcg: {}".format(epoch, scores["ndcg"][0]))
+        
+    return ndcgs, model
 
 def hyperparam_search():
-
-    epochs = 30
-    learning_rates = [10**-2, 10**-3]
+    # hyper-parameters
+    epochs = 20
+    learning_rates = [10**-2, 10**-1]
     n_hiddens = [150, 200, 250, 300, 350]
     sigmas = [10**-2, 10**-3]
 
@@ -97,7 +181,8 @@ def hyperparam_search():
                         labels = torch.tensor(data.train.label_vector[s_i:e_i])
 
                         model = train_batch(documentfeatures, labels, model, sigma)  
-              
+
+                                       
                     scores = eval_model(model, data.validation)
               
                     ndcg = scores["ndcg"][0]
@@ -112,53 +197,20 @@ def hyperparam_search():
                         print("Best parameters:", best_params)
     
     return best_params
-
-def train_best(best_params):
-    epochs = best_params["epoch"]
-    n_hidden = best_params["n_hidden"]
-    learning_rate = best_params["learning_rate"]
-    sigma = best_params["sigma"]
-    
-    model = Model(data.num_features, n_hidden, learning_rate, sigma)
-
-    arrs, ndcgs = [], []
-    for epoch in range(epochs):
-        eval_count = 0
-        for qid in range(0, data.train.num_queries()):
-            if data.train.query_size(qid) < 2:
-                continue
-            s_i, e_i = data.train.query_range(qid)
-            
-            documentfeatures = torch.tensor(data.train.feature_matrix[s_i:e_i]).float()
-            labels = torch.tensor(data.train.label_vector[s_i:e_i])
-            model = train_batch(documentfeatures, labels, model, sigma) 
-            eval_count +=1
-            if eval_count % 2000 == 0:
-                scores = eval_model(model, data.validation)
-                ndcgs.append(scores["ndcg"][0])
-                arrs.append(scores["relevant rank"][0])
-        print("Epoch: {}, ndcg: {}".format(epoch, scores["ndcg"][0]))
-        
-    return arrs, ndcgs, model
-
-def plot_ndcg_arr(losses, ndcgs):
-    x = np.arange(len(losses))
-    fig, ax = plt.subplots()
-    
-    ax.plot(x, losses, label='ARR')
-    ax.plot(x, ndcgs, label='NDCG')
-    ax.set_xlabel("Batch % 2000")
-    ax.set_ylabel("Score")
-    ax.set_title("RankNet LTR")
-    legend = ax.legend(loc='upper center')
-    
-    plt.show()
-    
+  
 
 if __name__ == "__main__":
     #determine best hyper parameters
     best_params = hyperparam_search()
     #train best model
     ndcgs, model = train_best(best_params)
+    #plot ndcg     
+    plot_ndcg_loss(ndcgs)
+    #get distributions of scores
+    distributions = get_distributions(model)
     #performance on test set
+    data = dataset.get_dataset().get_data_folds()[0]
+    data.read_data()
     scores = eval_model(model, data.test)
+    print('ndcg and err on test set for lambdarank:', scores['ndcg'], scores['err'])
+
